405b356793cde737ad44f183306588f7e3f8d231
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-05-01 16:53:30 +0530

    removing second cors

--- server/server.js
+++ server/server.js
@@ -4,9 +4,10 @@ import routes from "./routes/index.js"
 const app= express()
 const PORT =process.env.PORT || 5000
 
-app.use(cors());
+
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(cors());
+// app.use(express.urlencoded({extended:false}))
 //route file
 app.use(routes)
 
