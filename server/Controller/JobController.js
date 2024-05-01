ec417bebb2b0b713e31de5f276352a15fa1c00da
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-05-01 22:25:27 +0530

    fix server

--- server/Controller/JobController.js
+++ server/Controller/JobController.js
@@ -2,30 +2,32 @@ import prisma from '../DB/db.config.js';
 
 
 export const fetchJobs=async(req,res)=>{
-    const jobs=await prisma.job.findMany({})
-    return res.json({success:true, data:jobs,message:"Jobs fetched Successfully"})
+    // const jobs=await prisma.job.findMany({})
+    // return res.json({success:true, data:jobs,message:"Jobs fetched Successfully"})
+    res.json({success:true})
 
 }
 export const getJob=async(req,res)=>{
-    const jobId=req.params.id
+    // const jobId=req.params.id
 
-    const job=await prisma.job.findFirst({
-        where:{
-            id:Number(jobId)
-        }
-    })
-    return res.json({success:true, data:job,message:"Jobs fetched Successfully"})
+    // const job=await prisma.job.findFirst({
+    //     where:{
+    //         id:Number(jobId)
+    //     }
+    // })
+    // return res.json({success:true, data:job,message:"Jobs fetched Successfully"})
 
 }
 export const deleteJob=async(req,res)=>{
-    const jobId=req.params.id
+    res.json({success:true})
+    // const jobId=req.params.id
 
-    await prisma.job.delete({
-        where: {
-            id:Number(jobId)
-        }
-    })
-    return res.json({success:true,message:"Jobs Deleted Successfully"})
+    // await prisma.job.delete({
+    //     where: {
+    //         id:Number(jobId)
+    //     }
+    // })
+    // return res.json({success:true,message:"Jobs Deleted Successfully"})
 
 }
 
@@ -35,11 +37,11 @@ export const createJob = async (req, res) => {
 
     // Create the Job
     try {
-        await prisma.job.createMany({
-            data: {
-               ...rest
-            },
-        });
+        // await prisma.job.createMany({
+        //     data: {
+        //        ...rest
+        //     },
+        // });
         return res.status(200).json({ message: 'Job created successfully' });
     } catch (error) {
         console.log('Error creating Job:', error);
@@ -54,14 +56,14 @@ const jobId= req.params.id
 const {email,...rest}=req.body
 // console.log(...rest)
 
-await prisma.job.update({
-    where:{
-        id:Number(jobId)
-    },
-    data:{
-        ...rest
-    }
-})
+// await prisma.job.update({
+//     where:{
+//         id:Number(jobId)
+//     },
+//     data:{
+//         ...rest
+//     }
+// })
 
 return res.json({status:200, message:"Job updated successfully"})
 }
